3dfc4e76024093c068f53513d813cb9cf24437c6
Soonya <sdavydova0501@gmail.com>
2020-07-08 00:03:48 +0300

    Оживляем интерактивные элементы

---
+++ js/script.js
@@ -0,0 +1,28 @@
+const searchButton = document.querySelector(".button_search")
+const form = document.querySelector(".form")
+const search_hotel_location = form.querySelector(".search_hotel_location")
+const search_form_input = form.querySelector(".search_form_input")
+const form_number = form.querySelector(".form_number")
+const fields = form.querySelectorAll(".field")
+
+searchButton.addEventListener("click", () => {
+  form.style.visibility = "visible"
+})
+
+function checkOnValid() {
+  let isValid = true
+  for (var i = 0; i < fields.length; i++) {
+    fields[i].style.borderColor = "transparent"
+    if (!fields[i].value) {
+      isValid = false
+      console.log(fields[i]);
+      fields[i].style.border="1px solid red"
+    }
+  }
+
+  if(isValid) {
+    form.style.visibility = "hidden"
+  }
+
+  return isValid
+}
