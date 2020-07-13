b305d63b7eeebada49c57a7703913abce5d454a7
Soonya <sdavydova0501@gmail.com>
2020-07-13 20:38:31 +0300

    правки

--- js/script.js
+++ js/script.js
@@ -1,29 +1,31 @@
 // INDEX.HTML
-const searchButton = document.querySelector(".button_search")
-const form = document.querySelector(".form")
-const search_hotel_location = form.querySelector(".search_hotel_location")
-const search_form_input = form.querySelector(".search_form_input")
-const form_number = form.querySelector(".form_number")
-const fields = form.querySelectorAll(".field")
+const search_button = document.querySelector(".button_search");
+const form = document.querySelector(".form");
+const search_hotel_location = form.querySelector(".search_hotel_location");
+const search_form_input = form.querySelector(".search_form_input");
+const form_number = form.querySelector(".form_number");
+const fields = form.querySelectorAll(".field");
 
-searchButton.addEventListener("click", () => {
-  form.classList.remove("form_hidden")
-})
+form.classList.add("form_hidden");
+
+search_button.addEventListener("click", () => {
+  form.classList.remove("form_hidden");
+});
 
 function checkOnValid() {
-  let isValid = true
+  let isValid = true;
   for (var i = 0; i < fields.length; i++) {
-    fields[i].style.borderColor = "transparent"
+    fields[i].style.borderColor = "transparent";
     if (!fields[i].value) {
-      isValid = false
+      isValid = false;
       console.log(fields[i]);
-      fields[i].style.border="1px solid red"
+      fields[i].style.border="1px solid red";
     }
   }
 
   if(isValid) {
-    form.classList.add("form_hidden")
+    form.classList.add("form_hidden");
   }
 
-  return isValid
+  return isValid;
 }
